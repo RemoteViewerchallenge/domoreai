2225ce2cf2bec51c72f994a2868aa460fb42c2cb
guysilvermt <guysilvermt@gmail.com>
2025-11-16 16:57:56 -0700

    refactor: Remove legacy VFS service and router

--- apps/api/src/routers/index.ts
+++ apps/api/src/routers/index.ts
@@ -1,6 +1,5 @@
 import { createTRPCRouter } from '../trpc.js';
 import { gitRouter } from './git.router.js';
-import { vfsRouter } from './vfs.router.js';
 import { providerRouter } from './providers.router.js';
 import { roleRouter } from './role.router.js';
 
