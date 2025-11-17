449654abbf64032be504b8c25707789836837e5f
guysilvermt <guysilvermt@gmail.com>
2025-11-17 01:39:21 -0700

    refactor: Update main router index

--- apps/api/src/routers/index.ts
+++ apps/api/src/routers/index.ts
@@ -2,13 +2,13 @@ import { createTRPCRouter } from '../trpc.js';
 import { gitRouter } from './git.router.js';
 import { providerRouter } from './providers.router.js';
 import { roleRouter } from './role.router.js';
-import { vfsRouter } from './vfs.router.js';
 import { externalRouter } from './external.router.js';
+import { vfsRouter } from './vfs.router.ts';
 
 export const appRouter = createTRPCRouter({
   git: gitRouter,
   providers: providerRouter,
   role: roleRouter,
-  vfs: vfsRouter,
   external: externalRouter,
+  vfs: vfsRouter,
 });
