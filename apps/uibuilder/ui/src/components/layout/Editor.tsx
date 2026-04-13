4152dc6039a557fd0973cb901b555e587bcd0415
guysilvermt <guysilvermt@gmail.com>
2026-04-13 17:26:20 -0600

    grids

--- apps/uibuilder/ui/src/components/layout/Editor.tsx
+++ apps/uibuilder/ui/src/components/layout/Editor.tsx
@@ -4,16 +4,17 @@ import { TilingPanel } from './TilingPanel';
 import { CanvasGrid } from '../ui/CanvasGrid';
 import { Button } from '../elements/Button';
 import { Text } from '../elements/Text';
+import { GridLayout, GridCell } from './GridLayout';
 import { ContextMenu } from '../../core/ContextMenu';
 
 export const Editor = () => {
   return (
-    <CraftEditor resolver={{ TilingPanel, CanvasGrid, Button, Text, Box }}>
+    <CraftEditor resolver={{ TilingPanel, CanvasGrid, Button, Text, Box, GridLayout, GridCell }}>
       <ContextMenu>
         <Box sx={{ width: '100vw', height: '100vh', bgcolor: '#000' }}>
           <Frame>
             {/* id="ROOT" is critical for the ContextMenu add function */}
-            <Element is={CanvasGrid as any} canvas id="ROOT" rows={1} cols={1}>
+            <Element is={CanvasGrid as any} canvas id="ROOT">
               <TilingPanel />
             </Element>
           </Frame>
